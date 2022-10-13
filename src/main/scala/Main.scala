fc1e06727814619a996c416f1f1e9a649af80073
Maurice Flanagan <mflanagan@v-helmteam.us>
2022-10-13 10:19:10 -0400

    WIP experiment with flat mapping to generate cp/vr id pairs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,20 +6,26 @@ import Json4SSerde.JsonSerde
 
 case class CustomerProfile(id: Int, key: String, value: Double)
 case class VoterRecord(id: Int, key: String, value: Double)
+case class Match(cp_id: Int, vr_id: Int)
+case class Matches(cp_id: Int, vr_ids: List[Int])
 case class ResolvedProfile(cp_id: Int, vr_id: Int)
 
 object Main {
   import Serdes._
   implicit val customerProfileSerde = new JsonSerde[CustomerProfile]
   implicit val voterRecordSerde = new JsonSerde[VoterRecord]
+  implicit val matchSerde = new JsonSerde[Match]
+  implicit val matchesSerde = new JsonSerde[Matches]
   implicit val resolvedProfileSerde = new JsonSerde[ResolvedProfile]
   implicit val voterRecordMapSerde = new JsonSerde[Map[Int, VoterRecord]]
 
   val CustomerProfileTopic = "CustomerProfile"
   val VoterRecordTopic = "VoterRecord"
+  val MatchTopic = "Match"
+  val MatchesTopic = "Matches"
   val ResolvedProfileTopic = "ResolvedProfile"
 
-  def main(args: Array[String]) = {
+  def buildTopology() = {
     val builder = new StreamsBuilder
 
     val customerProfileStream = builder
@@ -43,13 +49,32 @@ object Main {
       ResolvedProfile(cp.id, vrMatch.id)
     }
 
-    val resolvedProfileStream = customerProfileStream
+    def joinMatches(cp: CustomerProfile, vrs: Map[Int, VoterRecord]): Matches = {
+      val vr_ids = vrs.keys.toList
+      Matches(cp.id, vr_ids)
+    }
+
+    val matchesStream = customerProfileStream
+      .selectKey((_, cp) => cp.key)
+      .join(voterRecordsByKeyTable)(joinMatches)
+      .selectKey((_, rp) => rp.cp_id)
+    matchesStream.to(MatchesTopic)
+
+    val matchStream = matchesStream
+      .flatMapValues(v => v.vr_ids.map(vr_id => Match(v.cp_id, vr_id)))
+    matchStream.to(MatchTopic)
+
+    customerProfileStream
       .selectKey((_, cp) => cp.key)
       .join(voterRecordsByKeyTable)(resolve)
       .selectKey((_, rp) => rp.cp_id)
-    resolvedProfileStream.to(ResolvedProfileTopic)
+      .to(ResolvedProfileTopic)
 
-    val topology = builder.build()
+    builder.build()
+  }
+
+  def main(args: Array[String]) = {
+    val topology = Main.buildTopology()
     val driver = new TopologyTestDriver(topology)
 
     val customerProfileInputTopic = driver.createInputTopic(
@@ -62,6 +87,18 @@ object Main {
       VoterRecordTopic,
       intSerde.serializer(),
       voterRecordSerde.serializer()
+    )
+
+    val matchesOutputTopic = driver.createOutputTopic(
+      MatchesTopic,
+      intSerde.deserializer(),
+      matchesSerde.deserializer()
+    )
+
+    val matchOutputTopic = driver.createOutputTopic(
+      MatchTopic,
+      intSerde.deserializer(),
+      matchSerde.deserializer()
     )
 
     val resolvedProfileOutputTopic = driver.createOutputTopic(
@@ -87,9 +124,26 @@ object Main {
 
     val resolvedProfiles = resolvedProfileOutputTopic.readValuesToList().toArray()
 
-    assert(resolvedProfiles(0) == ResolvedProfile(5,1))
-    assert(resolvedProfiles(1) == ResolvedProfile(6,2))
-    assert(resolvedProfiles(2) == ResolvedProfile(7,3))
-    assert(resolvedProfiles(3) == ResolvedProfile(8,4))
+    assert(resolvedProfiles(0) == ResolvedProfile(5, 1))
+    assert(resolvedProfiles(1) == ResolvedProfile(6, 2))
+    assert(resolvedProfiles(2) == ResolvedProfile(7, 3))
+    assert(resolvedProfiles(3) == ResolvedProfile(8, 4))
+
+    val matches = matchesOutputTopic.readValuesToList().toArray()
+
+    assert(matches(0) == Matches(5, List(1, 3)))
+    assert(matches(1) == Matches(6, List(2, 4)))
+    assert(matches(2) == Matches(7, List(1, 3)))
+    assert(matches(3) == Matches(8, List(2, 4)))
+
+    val matchList = matchOutputTopic.readValuesToList().toArray()
+    assert(matchList(0) == Match(5, 1))
+    assert(matchList(1) == Match(5, 3))
+    assert(matchList(2) == Match(6, 2))
+    assert(matchList(3) == Match(6, 4))
+    assert(matchList(4) == Match(7, 1))
+    assert(matchList(5) == Match(7, 3))
+    assert(matchList(6) == Match(8, 2))
+    assert(matchList(7) == Match(8, 4))
   }
 }
